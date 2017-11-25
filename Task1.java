32177a4226f45e70923fad516107c7d7d82ebdde
StevenCloud <stevencloud9@gmail.com>
2017-11-24 19:45:17 -0600

    Assignment 2

---
+++ Task1.java
@@ -0,0 +1,14 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		double x = 12.7;
+	    x--;
+	    int y = (int) Math.round(x);
+	    y -= 5;
+	    y++;
+	    System.out.printf("y = %d\n", y);
+
+	}
+
+}
---
+++ Task2.java
@@ -0,0 +1,11 @@
+
+public class Task2 {
+	public static void main(String[] args) {
+		  double x = 12.7743;
+		    x--;
+		    x += 2;
+		    System.out.printf("x = %.2f\n", x);
+
+	}
+
+}
