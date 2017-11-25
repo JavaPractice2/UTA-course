926e3b6f4a4943bb4c26422f60511eae21d88d64
StevenCloud <stevencloud9@gmail.com>
2017-11-24 23:52:49 -0600

    Assignment 2

---
+++ Task3.java
@@ -0,0 +1,14 @@
+
+public class Task3 {
+	public static void main(String[] args) {
+		 String a = "hello";
+		    a += "!";
+		    a += "123";
+		    int b = a.length();
+		    System.out.printf("a = %s\n", a);
+		    System.out.printf("The length of a is %d characters.\n", b);
+		    System.out.printf("The length of a is %d characters.\n", a.length());
+
+	}
+
+}
