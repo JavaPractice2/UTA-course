79be8c6bd29cc2f398828703c11de40b0841468d
StevenCloud <stevencloud9@gmail.com>
2017-11-25 00:05:26 -0600

    Assignment 1

---
+++ 1Task4.java
@@ -0,0 +1,15 @@
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		int base = 3;
+		int height = 5;
+		double area =(double) base * height/2;
+		System.out.println(area);
+		
+                       /* Code didn't originally work bc it said "int area = base *..." 
+                        and lacked the "double" function
+                        */
+	}
+
+}
