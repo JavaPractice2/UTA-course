688255f94af1ef086edc32526b70cc9678c93b4c
StevenCloud <stevencloud9@gmail.com>
2017-11-24 19:44:04 -0600

    First Assignment

---
+++ Task3.java
@@ -0,0 +1,13 @@
+
+public class Task3 {
+	
+	public static void main(String[] args) {
+		
+		
+		double side_length = 10.0;
+		double square_area = side_length * side_length;
+		System.out.println(square_area);
+
+	}
+
+}
---
+++ Task5.java
@@ -0,0 +1,14 @@
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		double length = 5.5;
+		int width = 4;
+		double area = (double) length * width;
+		double perimeter = (double)  2 * (length + width);
+		System.out.println(area);
+		System.out.println(perimeter);
+
+	}
+
+}
